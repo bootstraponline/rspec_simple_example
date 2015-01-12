b88c88e2defdec279bf08cee33a034e2e0fcd8a7
bootstraponline <code@bootstraponline.com>
2015-01-12 13:40:08 -0500

    Add simple rspec example

---
+++ spec/a_spec.rb
@@ -0,0 +1,9 @@
+describe 'a' do
+  it 'a 1' do
+    puts 'a 1'
+  end
+
+  it 'a 2' do
+    puts 'a 2'
+  end
+end
---
+++ spec/b_spec.rb
@@ -0,0 +1,9 @@
+describe 'b' do
+  it 'b 3' do
+    puts 'b 3'
+  end
+
+  it 'b 4' do
+    puts 'b 4'
+  end
+end
